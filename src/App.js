2f8e1f4441c42e7bc3592f44f95d7985864e7bb0
Augustika <augustika1524@gmail.com>
2024-02-15 19:29:42 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import './index.css';
 
-function App() {
+const App =() => {
+  const [advise,setAdvise]= useState('');
+
+
+  const fetchAdvise =async()=>{
+
+    try{
+      const res = await fetch('https://api.adviceslip.com/advice');
+      const output = await res.json();
+      setAdvise(output.slip.advice);
+      console.log(output.slip);
+    
+    }
+    catch(error){
+        console.log('Something went wrong');
+    }
+  }
+  
+  useEffect(()=>{
+ 
+    fetchAdvise();
+  },[]);
+
+  console.log("Render: ", advise);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <h1 className="heading">{advise}</h1>
+        <button className="button" onClick={fetchAdvise}>
+        <span>Generate a Quote!</span>
+        </button>
+      </div>
     </div>
   );
 }
 
 export default App;
+
